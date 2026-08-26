b3332965a7d5c945772d1112bd9b632aa1c646ea
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-08-26 15:53:40 +0500

    Understanding the URL parameters

--- cheftables/myapp/urls.py
+++ cheftables/myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns=[
     path("",views.home),
+    path("dishes/<str:dish>",views.menu_items,name="menu_items"),
 ]
--- cheftables/myapp/views.py
+++ cheftables/myapp/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to lemon squeezy")
+
+def menu_items(request,dish):
+    items={
+        "Pasta":"This kind of pasta is made up of",
+        "Cheesecake":"Cheesecake is a special type of dessert",
+    }
+    description=items[dish]
+    return HttpResponse(f"<h2>{dish}</h2>" + description)

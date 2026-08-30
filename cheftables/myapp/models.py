e70a0b027d24877fa2f29f4c175c8876a7f11c6b
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-08-30 21:01:39 +0500

    Understanding Models Architecture

--- cheftables/myapp/models.py
+++ cheftables/myapp/models.py
@@ -5,4 +5,7 @@ class Menu(models.Model):
     name=models.CharField(max_length=50)
     cuisine=models.CharField(max_length=50)
     price=models.IntegerField()
-    
+
+    def __str__(self):
+        return self.name + "," + self.cuisine
+

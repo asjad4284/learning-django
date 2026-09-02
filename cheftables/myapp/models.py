a25d234012ebcbcd97435aa581e3916fefe412b7
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-09-02 23:30:13 +0500

    Database configuration

--- cheftables/myapp/models.py
+++ cheftables/myapp/models.py
@@ -4,3 +4,6 @@ class Logger(models.Model):
     first_name=models.CharField(max_length=20)
     last_name=models.CharField(max_length=20)
     time_log=models.TimeField()
+
+    def __str__(self):
+        return self.first_name
